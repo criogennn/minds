da4b5ef12b5844ab3e6d856a6d3d877fb4965d4f
Strygwyr <108073234+criogennn@users.noreply.github.com>
2022-06-23 16:11:42 +0300

    Update collector.py

--- collector.py
+++ collector.py
@@ -4,10 +4,10 @@ from web3 import Web3
 goerli_url = "https://eth-goerli.alchemyapi.io/v2/PFA1dZBZ9PuLICvNABBhhTi1yxLh2UcJ"
 web3 = Web3(Web3.HTTPProvider(goerli_url))
 
-address_1 = "0x2ad3E7145f7c4a62078052c4358d8671C8ddCA31"
-address_2 = "0x21e2663D0DB4b3EB1E6da33b45efF20608972d49"
+address_1 = "add1"
+address_2 = "add2"
 
-private_key = "a2c48afa28ca3e968430b7a82e448a58366fd999ff0b009c7bd97106bcfffeb3"
+private_key = "you key"
 
 nonce = web3.eth.getTransactionCount(address_1)
 
